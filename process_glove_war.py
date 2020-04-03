c837a85556fedd48770718f7787d755cb0640c2a
diegodebrito <diegosiebra13@gmail.com>
2020-04-03 17:30:13 +0000

    update all models

--- process_glove_war.py
+++ process_glove_war.py
@@ -1,21 +1,25 @@
-
 import pickle
+import pathlib
 import numpy as np
-
 
 # Parsing GloVe file
 embedding_index = {}
+
+# Local directory
+# glove_path = pathlib.Path(r'C:/Users/USUARIO/glove')
+# with open(glove_path / 'glove.6B.100d.txt', encoding='UTF-8') as f:
+# GCP
 with open('./glove/glove.6B.100d.txt', encoding='UTF-8') as f:
     for line in f:
         values = line.split()
         word = values[0]
         coefs = np.asarray(values[1:], dtype='float32')
         embedding_index[word] = coefs
-
+        
 # Building embedding matrix for our vocabulary
-with open("idx2word.pkl", "rb") as f:
+with open("idx2word", "rb") as f:
     idx2word = pickle.load(f)
-
+    
 EMBEDDING_DIM = 100
 
 n_matched = 0
@@ -29,12 +33,3 @@ print(n_matched)
 
 # Saving embedding matrix
 np.save('./embedding_matrix_war', embedding_matrix)
-
-
-
-
-
-
-
-
-
